93048115823c23dff227b83410098cc60cb54927
CAIA1 <kaisa.kuos@gmail.com>
2023-04-10 19:24:37 +0400

    folder reorganization and new file for reviews

---
+++ reviews.js
@@ -0,0 +1,27 @@
+export const customerReviews = [
+    {
+        "name": "Emily Smith" ,
+        "role": "Business Owner",
+        "review": "I've been using this banking app for my business for a few months now and it has completely transformed the way I manage my finances. The app is user-friendly and provides real-time updates on my transactions. I highly recommend it."
+    },
+    {
+        "name": "John Brown" ,
+        "role": "Freelancer",
+        "review": "As a freelancer, I need a banking app that is easy to use and secure. This app ticks both those boxes and more. I particularly like the budgeting feature which helps me keep track of my spending. It's a game changer for me."
+    },
+    {
+        "name": "David Lee",
+        "role": "Financial Controller",
+        "review": "As a financial controller, I've seen many banking apps come and go, but this one has truly impressed me. It offers a wide range of features that make managing my finances a breeze. The reporting feature is particularly useful as it allows me to generate reports quickly and easily."
+    },
+    {
+        "name": "Sarah Johnson",
+        "role": "Finance Admin",
+        "review": "As a finance admin, I use this banking app on a daily basis and it has made my job so much easier. I can easily approve transactions, monitor account balances, and even set up recurring payments. The app is very user-friendly and I would highly recommend it to anyone."
+    },
+    {
+        "name": "Michael Stone",
+        "role": "Office Assistant",
+        "review": "This banking app is great for someone like me who is always on the go. I can quickly and easily check my account balances, transfer funds, and pay bills all from my phone. The app is also very secure, which is important to me. Overall, I'm very satisfied with this app."
+    }
+]
